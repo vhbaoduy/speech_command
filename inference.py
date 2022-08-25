fix error in train phase and add cal variance

inference.py
@@ -77,7 +77,7 @@ def main():
             targets = targets.cpu().numpy().ravel()
             feats = feats.cpu().numpy()
 
-            for i in range(len(batch)):
+            for i in range(len(batch['path'])):
                 file_name = batch['path'][i]
                 name_class = utils.index_to_label(CLASSES, targets[i])
                 folder = os.path.join(conf.inference_path, name_class)
@@ -93,8 +93,6 @@ def main():
                 else:
                     np.save(os.path.join(folder, audio_name + ".npy"), feats[i])
 
-                break
-            break
 
 
 if __name__ == '__main__':

train.py
@@ -25,7 +25,7 @@ def get_lr(opt):
 
 
 def main():
-    global best_accuracy, global_step, start_epoch
+    global best_accuracy, global_step, start_epoch, start_timestamp, best_loss
     parser = argparse.ArgumentParser(description='Train model for speech command',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--config_file', type=str, default='./configs/configs.yaml')
@@ -208,7 +208,7 @@ def main():
                 correct += pred.eq(targets.data.view_as(pred)).sum()
                 total += targets.size(0)
 
-                writer.add_scalar('%s/loss' % phase, loss.data[0], global_step)
+                writer.add_scalar('%s/loss' % phase, loss.item(), global_step)
 
                 # update the progress bar
                 pbar.set_postfix({
@@ -234,12 +234,12 @@ def main():
             best_accuracy = accuracy
             torch.save(save_checkpoint,
                        conf.checkpoint_path + '/' + 'best-loss-speech-commands-checkpoint-%s.pth' % name)
-            torch.save(model, conf.checkpoint_path + '/' + '%d-%s-best-loss.pth' % (start_timestamp, name))
+            torch.save(model, conf.checkpoint_path + '/' + 'best-loss.pth' % (start_timestamp, name))
         if epoch_loss < best_loss:
             best_loss = epoch_loss
             torch.save(save_checkpoint,
                        conf.checkpoint_path + '/' + 'best-acc-speech-commands-checkpoint-%s.pth' % name)
-            torch.save(model, conf.checkpoint_path + '/' + '%d-%s-best-acc.pth' % (start_timestamp, name))
+            torch.save(model, conf.checkpoint_path + '/' + 'best-acc.pth' % (start_timestamp, name))
 
         torch.save(save_checkpoint, conf.checkpoint_path + '/' + 'last-speech-commands-checkpoint.pth')
         del checkpoint  # reduce memory

utils/utils.py
@@ -1,5 +1,7 @@
+import os.path
 import random
 import librosa
+import numpy as np
 
 
 def label_to_index(labels: object, label: str):
@@ -22,7 +24,7 @@ def index_to_label(labels: object, index: int):
     return labels[index]
 
 
-def load_audio(path:str, sample_rate:int):
+def load_audio(path: str, sample_rate: int):
     """
     Load audio from path with sample rate
     :param path: Path to audio
@@ -37,5 +39,36 @@ def load_audio(path:str, sample_rate:int):
 
 def is_apply_transform(prob=0.5):
     return random.random() < prob
+
+
+def load_features(path: str, n=None):
+    if os.path.isdir(path):
+        vocab_name = path.split('\\')[-1]
+        features = []
+        for file_name in os.listdir(path):
+            if file_name.endswith('.npy'):
+                feat = np.load(os.path.join(path, file_name))
+                features.append(feat)
+                if n is not None and len(features) == n:
+                    break
+        return vocab_name, np.array(features), len(features)
+
+    return None, None, None
+
+
+def compute_variance(features):
+    mean = np.mean(features, axis=0)
+    norm = features - mean
+    variance = np.mean(norm ** 2)
+    radius = np.max(np.linalg.norm(norm, axis=0))
+    return variance, radius
+
+
+def visualize(feature_path, classes, n_class=5, n_samples=100):
+    random_choice = []
+    while len(random_choice) < n_class:
+        choice = random.choice(classes)
+        if choice not in random_choice:
+            random_choice.append(choice)
 
 